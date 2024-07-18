0503ed48d361488c50042a49c19195c73dfc2bf5
Melanie Mai <melchmai@gmail.com>
2024-07-18 12:54:35 -0300

    fix

--- react/src/index.js
+++ react/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createPopper } from '@popperjs/core';
 
 window.$ = $;
